124243f81fde107ff6632cc4fcb5b0bd35a6f2aa
Ethosa <social.ethosa@gmail.com>
2023-12-15 21:15:17 +0700

    upgrade android building

--- tests/android_tests/android/app/src/main/java/com/hapticx/tmpl/MainActivity.java
+++ tests/android_tests/android/app/src/main/java/com/hapticx/tmpl/MainActivity.java
@@ -1,16 +1,14 @@
 package com.hapticx.tmpl;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private WebView w;
     private boolean paused = false;
 
