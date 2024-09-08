e168cecf414f64c5024de5b9fa975b7be9cba7a3
Ethosa <social.ethosa@gmail.com>
2024-09-08 14:34:23 +0700

    upgrade android template

--- src/happyx_native/android/tmpl/app/src/main/java/com/hapticx/tmpl/MainActivity.java
+++ src/happyx_native/android/tmpl/app/src/main/java/com/hapticx/tmpl/MainActivity.java
@@ -2,15 +2,11 @@ package com.hapticx.tmpl;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import androidx.webkit.WebSettingsCompat;
-import androidx.webkit.WebViewFeature;
 
 public class MainActivity extends Activity {
     private WebView w;
@@ -21,14 +17,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle s) {
         super.onCreate(s);
         setContentView(R.layout.activity_main);
-
+        getActionBar().hide();
         w = findViewById(R.id.webView);
 
         if (!paused) {
             new Thread(() -> Native.start(this)).start();
 
             Native n = new Native(this);
-            n.uiLoop();
+            new Thread(n::uiLoop).start();
         }
         setupWebView();
     }
@@ -43,24 +39,13 @@ public class MainActivity extends Activity {
                 super.onPageFinished(v, u);
             }
         });
-        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
-            switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
-                case Configuration.UI_MODE_NIGHT_YES:
-                    WebSettingsCompat.setForceDark(w.getSettings(), WebSettingsCompat.FORCE_DARK_ON);
-                    break;
-                case Configuration.UI_MODE_NIGHT_NO:
-                case Configuration.UI_MODE_NIGHT_UNDEFINED:
-                    WebSettingsCompat.setForceDark(w.getSettings(), WebSettingsCompat.FORCE_DARK_OFF);
-                    break;
-            }
-        }
 
         WebSettings ws = w.getSettings();
         ws.setJavaScriptEnabled(true);
         ws.setDomStorageEnabled(true);
         ws.setBuiltInZoomControls(true);
         ws.setDisplayZoomControls(false);
-        ws.setSupportZoom(true);
+        ws.setSupportZoom(false);
         ws.setDefaultTextEncodingName("utf-8");
 
         w.loadUrl("http://localhost:15123/");
